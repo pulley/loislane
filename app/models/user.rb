7d87c71fe9b454fbf884e43965fdc18e907f80f5
Tony Coconate <me@tonycoconate.com>
2011-02-01 09:43:16 -0600

    User model fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,20 +11,18 @@ class User < ActiveRecord::Base
   def self.authenticate(username, password)
     return nil if username.blank? || password.blank?
 
-    if user = User.where(:username => username).first
-      return user
-    else
-      Net::LDAP.open(:host => "ttwpusaldap01.usa.tribune.com", :port => 389, :base => "dc=usa,dc=tribune,dc=com") do |ldap|
-        if ldap.bind(:username => "usa\\#{username}", :password => password, :method => :simple)
-          unless user
-            user = User.create!(:username => username, :email => "#{username}@tribune.com")
-          end
+    Net::LDAP.open(:host => "ttwpusaldap01.usa.tribune.com", :port => 389, :base => "dc=usa,dc=tribune,dc=com") do |ldap|
+      if ldap.bind(:username => "usa\\#{username}", :password => password, :method => :simple)
+        user = User.where(:username => username).first
 
-          return user
-        else
-          Rails.logger.error("LDAP: Error #{ldap.get_operation_result.code}")
-          return nil
+        unless user
+          user = User.create!(:username => username, :email => "#{username}@tribune.com")
         end
+
+        return user
+      else
+        Rails.logger.error("LDAP: Error #{ldap.get_operation_result.code}")
+        return nil
       end
     end
   end
