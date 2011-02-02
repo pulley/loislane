7dfb029f45cfa9ffd22e67ddd5a37cbc53311855
Tony Coconate <me@tonycoconate.com>
2011-02-02 14:22:45 -0600

    deployment changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+require 'bundler/capistrano'
+
 default_run_options[:pty] = true  # Must be set for the password prompt from git to work
 
 set :application, "loislane"
@@ -8,6 +10,9 @@ set :user, "tribunedev"
 set :runner, user
 set :use_sudo, false
 set :deploy_to, "/Users/#{user}/Sites/#{application}"
+
+set :bundle_cmd, "/Users/#{user}/.rvm/gems/ree-1.8.7-2010.02/bin/bundle"
+set :bundle_without, [:development, :test]
 
 role :web, "xserve.tii.trb"                          # Your HTTP server, Apache/etc
 role :app, "xserve.tii.trb"                          # This may be the same as your `Web` server
