643de74d6dec87ca99f36832f2b611d74f387830
Tony Coconate <me@tonycoconate.com>
2011-02-02 23:41:22 -0600

    Skipping stuff

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -1,5 +1,6 @@
 class CommitsController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:create]
+  skip_before_filter :authenticate_user!, :only => [:create]
 
   before_filter :verify_ownership, :only => [:edit, :update, :destroy]
 
