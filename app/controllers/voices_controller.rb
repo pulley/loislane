ee524fa52e046119adbf96207b38f87d0c80c2f1
Tony Coconate <me@tonycoconate.com>
2011-02-18 11:37:39 -0600

    changes for destroys

--- app/controllers/voices_controller.rb
+++ app/controllers/voices_controller.rb
@@ -1,28 +1,24 @@
 class VoicesController < ApplicationController
   respond_to :js
 
-  before_filter :setup_commit
-
   # POST /commits/1/voices
   def create
+    @commit = Commit.find(params[:commit_id])
+
     @voice = Voice.new(params[:voice])
     @voice.user = current_user
     @voice.commit = @commit
     @voice.save
+
     respond_with @voice
   end
 
   # DELETE /commits/1/voices/1
   def destroy
     @voice = Voice.find(params[:id])
+
     @voice.destroy
 
     respond_with @voice
   end
-
-  private
-
-    def setup_commit
-      @commit = Commit.find(params[:commit_id])
-    end
 end
